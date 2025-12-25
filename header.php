fa92d0e247b22a63c56f7f9f19f8f0dca2fe6f54
Faijur199 <faijurahaman4@gmail.com>
2025-12-25 07:31:57 +0545

    WIP

--- header.php
+++ header.php
@@ -15,7 +15,8 @@
                 <div class="layout-content-container flex flex-col max-w-[960px] flex-1 px-4 sm:px-10">
 
                     <header class="flex items-center justify-between whitespace-nowrap border-b border-solid border-gray-200 dark:border-gray-800 py-4">
-                        <div class="flex items-center gap-4">
+                        <a href="<?php echo home_url() ?>">
+                            <div class="flex items-center gap-4">
                             <div class="size-6 text-primary">
                                 <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
                                     <g clip-path="url(#clip0_6_330)">
@@ -30,6 +31,7 @@
                             </div>
                             <h2 class="text-lg font-bold leading-tight tracking-[-0.015em]">Olivia Chen</h2>
                         </div>
+                        </a>
                         <div class="hidden sm:flex flex-1 justify-end gap-8">
                             <div class="flex items-center gap-9">
                                 <a class="text-sm font-medium leading-normal hover:text-primary transition-colors" href="#">Work</a>
